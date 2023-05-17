a31afd51127b36cf93b615ab0c446111c5a10acb
Petro Mozil <mozil.petryk@gmail.com>
2023-05-17 09:07:24 +0300

    Add indices to the suffix tree
    It now eats N² space, xoxo

--- src/suffix_tree/suffix_tree_basic.py
+++ src/suffix_tree/suffix_tree_basic.py
@@ -7,6 +7,19 @@ The suffix tree is used to search for substring in linear time
 from ..b_tree import BTreeNode, IterableWithLen, NodeValue
 
 
+class SufTreeNode(BTreeNode):
+    """
+    Suffix tree node
+    """
+
+    def __init__(self, val: NodeValue | None = None, idx: int = -1):
+        """
+        Init for suffix tree node
+        """
+        super().__init__(val)
+        self.indices: list[int] = [idx]
+
+
 class BasicSuffixTree:
     """
     The suffix tree class.
@@ -16,27 +29,30 @@ class BasicSuffixTree:
         """
         Init for the basic suffix tree
         """
-        self.root = BTreeNode()
+        self.root = SufTreeNode()
         if text is not None:
             self.root = self.build_from_iterable(text)
 
     def build_from_iterable(
         self, iterable: IterableWithLen[NodeValue]
-    ) -> BTreeNode[NodeValue]:
+    ) -> SufTreeNode:
         """
         Build the tree from iterable
         """
-        root = BTreeNode()
-        for suffix in self.suffixes(iterable):
-            cur: BTreeNode = root
+        root = SufTreeNode()
+        for suffix, start in self.suffixes(iterable):
+            cur: SufTreeNode = root
             idx = 0
             while idx < len(suffix):
                 val = cur[suffix[idx]]
                 if val is not None:
                     cur = val
                 else:
-                    cur.add_child(BTreeNode(suffix[idx]))
-                    cur: BTreeNode = cur[suffix[idx]]
+                    cur.add_child(SufTreeNode(suffix[idx], start + idx))
+                    cur = cur[suffix[idx]]
+
+                if start + idx not in cur.indices:
+                    cur.indices.append(start + idx)
                 idx += 1
         return root
 
@@ -45,7 +61,7 @@ class BasicSuffixTree:
         """
         Get all the suffixes from the iterable
         """
-        return [iterable[start:] for start in range(len(iterable))]
+        return [(iterable[start:], start) for start in range(len(iterable))]
 
     @property
     def leaves(self) -> int:
