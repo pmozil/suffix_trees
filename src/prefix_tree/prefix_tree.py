c0d81868bb5448ea593da5e15e78d404fd0c733c
Oleksandr Ivaniuk <ivaniuk.pn@ucu.edu.ua>
2023-05-19 15:54:32 +0300

    str fot tree

--- src/prefix_tree/prefix_tree.py
+++ src/prefix_tree/prefix_tree.py
@@ -8,7 +8,7 @@ class PrefixTree:
     """
     def __init__(self) -> None:
         self._head = Node(letters=[])
-    
+
     @staticmethod
     def get_letter(value, letters):
         '''Return a Letter with value if it is inside. Else None.'''
@@ -20,16 +20,15 @@ class PrefixTree:
         """
         Add word to the tree.
         """
-        current_node = self._head if node is None else node # if it is the first letter of a word -> None
+        # if it is the first letter of a word -> None
+        current_node = self._head if node is None else node
         letter = self.get_letter(word[0], current_node.letters)
-        print(current_node)
 
         if len(word) == 1: # it is the end of the word
             if letter: # the word is already exist
                 letter.is_end = True
             else:
                 current_node.letters.append(Letter(word[0], is_end = True))
-            print(current_node)
         else:
             if letter: # the part of the word is already exist
                 if not letter.child:
@@ -37,23 +36,32 @@ class PrefixTree:
 
                 next_node = letter.child
 
-                print(current_node)
-                print(next_node)
-
                 self.add_word(word[1:], next_node)
             else:
                 new_letter = Letter(word[0], child = Node(letters=[]))
                 current_node.letters.append(new_letter)
 
-                print(new_letter.child.letters is current_node.letters)
-                '''Леттер має порожній нод, але як тільки я в каррент роблю
-                аппенд, то обидва ноди отримують леттер.'''
-
-                print(current_node)
-
                 next_node = new_letter.child
 
                 self.add_word(word[1:], next_node)
+
+    def __str__(self):
+        """Returns a string representation with the tree rotated
+        90 degrees counterclockwise."""
+
+        def recurse(node, level):
+            res = ""
+            if node is not None:
+                for letter in node.letters:
+                    if letter.child:
+                        res += recurse(letter.child, level + 1)
+                    if letter.is_end:
+                        res += "\n" + " |" * level + str(letter.value) + "."
+                    else:
+                        res += "\n" + " |" * level + str(letter.value) + " "
+            return res
+
+        return recurse(self._head, 0)
 
 class Node:
     """
@@ -65,8 +73,7 @@ class Node:
     def __str__(self):
         res = ''
         for let in self.letters:
-            res += ' ' + str(let)
-
+            res += '|' + str(let)
         return res
 
 class Letter:
@@ -77,7 +84,14 @@ class Letter:
         self.value = value # letter
         self.child = child # Node(letters=[])
         self.is_end = is_end # if the letter can be the end of a word
-    
+
     def __str__(self):
         return str(self.value)
- 
+
+tree = PrefixTree()
+tree.add_word('cat')
+tree.add_word('car')
+tree.add_word("dog")
+tree.add_word('drug')
+tree.add_word('carrot')
+print(tree)
