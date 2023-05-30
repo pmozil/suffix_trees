a3937b07f36e3edafa33f6ad7b22f7f9ad16c5d0
KateKoval <kovalchuk.pn@ucu.edu.ua>
2023-05-30 10:13:03 +0300

    Ukkonen does not work

--- src/suffix_tree/suffix_tree_ukonen.py
+++ src/suffix_tree/suffix_tree_ukonen.py
@@ -28,6 +28,11 @@ class UkkonenTreeNode:
             children={self.children}
         )
         """
+    
+    def __hash__(self) -> int:
+        """Hask method for the node
+        """
+        return hash((self.start, self.end[0], self.idx))
 
 
 class UkkonenTree:
@@ -43,8 +48,8 @@ class UkkonenTree:
         self.active_edge = -1
         self.active_length = 0
         self.remainning_suffixes = 0
-        self._leaf_end = -1
-        self.root_end = []
+        self.leaf_end = [0]
+        self.root_end = [0]
         self.split_end = -1
         self.size = -1
         self.build_tree()
@@ -72,55 +77,52 @@ class UkkonenTree:
         return False
 
     def extend_suffix_tree(self, pos: int):
-        self.root_end[0] = pos
+        self.leaf_end[0] = pos
         self.remainning_suffixes += 1
         self.last_new_node = None
         while self.remainning_suffixes > 0:
-            
-            if self.active_length > 0:
+            if self.active_length == 0:
                 self.active_edge = pos
-
             char = self.text[self.active_edge]
-
             if (
                 self.active_node is not None
                 and self.active_node.children.get(char) is None
             ):
-                self.active_node.children[char] = self.new_node(pos, self.root_end)
+                self.active_node.children[char] = self.new_node(pos, self.leaf_end)
+                
                 if self.last_new_node is not None:
                     self.last_new_node.link = self.active_node
                     self.last_new_node = None
-
+            
             elif self.active_node is not None:
                 next = self.active_node.children.get(char)
+                
                 if self.walk_down(next):
                     continue
-
+                
                 if self.text[next.start + self.active_length] == self.text[pos]:
                     if self.last_new_node is not None and self.active_node != self.root:
                         self.last_new_node.link = self.active_node
                         self.last_new_node = None
                     self.active_length += 1
                     break
-
                 tmp = next.start + self.active_length - 1
-                self.split_end = tmp
-                split = self.new_node(next.start, self.root_end)
+                split_end = [tmp]
+                split = self.new_node(next.start, split_end)
+                self.active_node.children[char] = split
+                split.children[char] = self.new_node(pos, self.leaf_end)
                 next.start += self.active_length
                 split.children[self.text[next.start]] = next
-
                 if self.last_new_node is not None:
                     self.last_new_node.link = split
-
                 self.last_new_node = split
-
             self.remainning_suffixes -= 1
             if self.active_node == self.root and self.active_length > 0:
                 self.active_length -= 1
                 self.active_edge = pos - self.remainning_suffixes + 1
-            elif self.active_node != self.root and self.active_node is not None:
-                self.active_node = self.active_node.link
-
+            elif self.active_node != self.root and self.active_node:
+                self.active_node = self.active_node.link if self.active_node is not None else self.root
+            
     def build_tree(self):
         self.size = len(self.text)
         tmp = -1
@@ -132,9 +134,28 @@ class UkkonenTree:
 
     def indices(self, substr: str):
         word = substr
-        return 0
+        idx = 0
+        cur = self.root
+        while cur is not None and idx < len(word):
+            char = word[idx]
+            cur = cur.children.get(char)
+            idx += 1
+        if cur is None:
+            return []
+        stack = [cur]
+        visited = {cur}
+        indices = [cur.start]
+        while stack:
+            cur = stack.pop()
+            print(cur.children)
+            for child in cur.children.values():
+                if child not in visited:
+                    print(1)
+                    stack.append(child)
+                    indices.append(child.start)
+        return indices
 
-import timeit
-with open(__file__, 'r') as f:
-    a = timeit.timeit(lambda: UkkonenTree(f.read()), number=10000)
-    print(a)
+# import timeit
+# with open(__file__, 'r') as f:
+#     a = timeit.timeit(lambda: UkkonenTree(f.read()), number=10000)
+#     print(a)
